f6ca74c3e0135d6b1f9b730edab80c4b7598124e
anirudtate <anirud.programming@gmail.com>
2022-05-09 11:53:27 +0530

    added swallow patch

--- config.h
+++ config.h
@@ -2,19 +2,25 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 0;        /* gaps between windows */
+static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrains Mono:size=10" };
 static const char dmenufont[]       = "JetBrains Mono:size=10";
-static char normbgcolor[]           = "#1d2021";
-static char normbordercolor[]       = "#1d2021";
-static char normfgcolor[]           = "#689d6a";
-static char selfgcolor[]            = "#1d2021";
-static char selbordercolor[]        = "#928374";
+static char normbgcolor[]           = "#000000";
+static char normbordercolor[]       = "#000000";
+static char selfgcolor[]            = "#000000";
+static char normfgcolor[]           = "#458588";
+static char selbordercolor[]        = "#458588";
+static char selbgcolor[]            = "#458588";
+
+
+/* static char normfgcolor[]           = "#8ec07c"; */
+/* static char selbordercolor[]        = "#8ec07c"; */
 /* static char selbgcolor[]            = "#8ec07c"; */
-static char selbgcolor[]            = "#928374";
+/* static char selbgcolor[]            = "#928374"; */
 static const unsigned int baralpha = 0xe6;
 static const unsigned int borderalpha = OPAQUE;
 
@@ -40,9 +46,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
